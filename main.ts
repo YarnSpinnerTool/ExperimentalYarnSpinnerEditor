86de8e40510b1027c8f2b38faba4e8f70f21aaac
Seth Hilder <seth.hilder@eightyoptions.com>
2021-04-12 10:07:11 +1000

    #1 - Demo of eslint

--- main.ts
+++ main.ts
@@ -1,30 +1,35 @@
 import { app, BrowserWindow } from "electron";
 import * as path from "path";
 
+/**
+ * Creates the main window
+ * 
+ * @returns {null} No return
+ */
 function createWindow() {
-	// Create the browser window.
-	const mainWindow = new BrowserWindow({
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
 	  height: 600,
 	  webPreferences: {
 		  preload: path.join(__dirname, "src/ts/preload.js"),
-		},
-		width: 800,
-	});
+        },
+        width: 800,
+    });
 	
-	// and load the index.html of the app.
-	mainWindow.loadFile(path.join(__dirname, "../src/html/index.html"));
+    // and load the index.html of the app.
+    mainWindow.loadFile(path.join(__dirname, "../src/html/index.html"));
 }
 
 app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") {
-		app.quit();
-	}
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
 });
 
 app.on("activate", () => {
-	if (BrowserWindow.getAllWindows().length === 0) {
-		createWindow();
-	}
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });

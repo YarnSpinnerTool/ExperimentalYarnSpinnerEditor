b06e9cab5f887654bb27f655211007d61aa60087
Seth Hilder <setho246@gmail.com>
2021-04-19 09:03:17 +1000

    #6 - Remove CleanWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const {merge} = require('webpack-merge')
 
 const loaders = {
@@ -26,9 +25,6 @@ const loaders = {
   
 const commonConfig = { 
 	mode: 'development',
-	plugins: [
-		new CleanWebpackPlugin(),
-	]
 }
 
 const renderConfig = merge(commonConfig, {

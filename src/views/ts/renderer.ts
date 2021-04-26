6fee0b4898d5772d3ca9cb67df3d4b5338c9b26a
Cullie <cullie.02@gmail.com>
2021-04-26 23:58:33 +1000

    #11 - Fixed auto-resizing
    Fixed auto-resizing. Added a cute theme and large font to suit our needs, will be changed in time.

--- src/views/ts/renderer.ts
+++ src/views/ts/renderer.ts
@@ -15,29 +15,26 @@ if(document!) {
 			return '../dist/editor.worker.js';
 		}
 	};
-	
-	monaco.editor.create(document.getElementById('container')!, {
-		theme: 'vs-dark',
-		value: [`
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process unless
-// nodeIntegration is set to true in webPreferences.
-// Use preload.js to selectively enable features
-// needed in the renderer process.
-import * as monaco from 'monaco-editor';
-if(document!) {
-	// @ts-ignore
-	self.MonacoEnvironment = {
-		getWorkerUrl: function (moduleId: String, label: String) {
-			if (label === 'typescript' || label === 'javascript') {
-				return '../../../dist/ts.worker.render.js';
-			}
-			return '../../../dist/editor.worker.js';
+	monaco.editor.defineTheme('yarnSpinnerTheme', {
+		base: 'vs',
+		inherit: true,
+		rules: [{ background: 'EDF9FA' }],
+		colors: {
+			'editor.foreground': '#000000',
+			'editor.background': '#EDF9FA',
+			'editorCursor.foreground': '#8B0000',
+			'editor.lineHighlightBackground': '#0000FF20',
+			'editorLineNumber.foreground': '#008800',
+			'editor.selectionBackground': '#88000030',
+			'editor.inactiveSelectionBackground': '#88000015'
 		}
-	};
-}`
-		].join('\n'),
+	});
+	monaco.editor.create(document.getElementById('container')!, {
+		theme: 'yarnSpinnerTheme',
+		value: [].join('\n'),
 		language: 'typescript',
+		automaticLayout: true,
+		fontFamily: "Arial",
+		fontSize: 20
 	});
 }

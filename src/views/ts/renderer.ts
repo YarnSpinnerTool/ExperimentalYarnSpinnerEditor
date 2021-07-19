1c71d697a2a1d4491ece99da628cc3311346637f
Cullie <cullie.02@gmail.com>
2021-07-19 12:42:27 +1000

    #27 - Konva stage, CSS broken

--- src/views/ts/renderer.ts
+++ src/views/ts/renderer.ts
@@ -159,6 +159,37 @@ import * as monaco from "monaco-editor";
 import * as yarnSpinner from "../../YarnSpinner/yarnSpinnerMonarch";
 import { ipcRenderer } from "electron";
 import exports from "../../controllers/themeReader.ts";
+import Konva from 'konva';
+
+var stage = new Konva.Stage({
+    container: 'nodeContainer',   // id of container <div>
+    width: 500,
+    height: 1000
+  });
+  
+  // then create layer
+  var layer = new Konva.Layer();
+  
+  // create our shape
+  var rect = new Konva.Rect({
+    x: stage.width() / 2,
+    y: stage.height() / 2,
+    width: 70,
+    height: 70,
+    fill: '#f5f0b0',
+    stroke: '#f2deac',
+    strokeWidth: 1,
+    draggable: true,
+  });
+  
+  // add the shape to the layer
+  layer.add(rect);
+  
+  // add the layer to the stage
+  stage.add(layer);
+  
+  // draw the image
+  layer.draw();
 
 const yarnFileManager = new YarnFileManager();
 

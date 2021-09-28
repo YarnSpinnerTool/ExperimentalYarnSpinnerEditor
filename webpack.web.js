018430a31c29c7559d1f4690ce7a55fe0dfd1003
Seth Hilder <setho246@gmail.com>
2021-09-28 18:51:14 +1000

    #122 - Create new webpack config

---
+++ webpack.web.js
@@ -0,0 +1,51 @@
+path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: './src/renderer.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+              },
+              {
+                test: /\.png$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'img/[name].[ext]',
+                            publicPath: './'
+                        }
+                    },
+                ],
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Custom template',
+            // Load a custom template (lodash by default)
+            template: './src/index.html'
+          }),
+          new CopyWebpackPlugin({
+            patterns: [{ from: path.resolve(__dirname, 'assets', 'icons'), to: 'img' }],
+          })
+        
+    ]
+    
+};
